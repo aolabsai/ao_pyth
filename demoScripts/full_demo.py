c012386d83cb40d805feaf70e7d909f05677937c
Rafayel Latif <rafayel.latif@gmail.com>
2025-03-16 08:46:02 +0000

    WIP add endpoint arg to switch between dev and prod

--- demoScripts/full_demo.py
+++ demoScripts/full_demo.py
@@ -1,19 +1,17 @@
-import numpy as np
-
 import ao_python as ao
 from config import api_key
 
 
 # Creating an Agent Architecture
 arch = ao.Arch(arch_i="[1, 1, 1]", arch_z="[1]", connector_function="full_conn",
-                  api_key=api_key, kennel_id="full_demo")
+                  api_key=api_key, kennel_id="full_demo", endpoint="dev")
 # checking if the Arch has been successfully created
 print(arch.api_status)
 
 
 # Creating an Agent using that Arch
 agent = ao.Agent(Arch=arch,
-                 api_key=api_key, uid="full_demo_agent")
+                 api_key=arch.api_key, uid="full_demo_agent", endpoint="dev")
 ## If you don't have an `Arch` variable in your runtime, you can slso create/invoke Agents by entering an `api_key` and `kennel_id`
 
 
